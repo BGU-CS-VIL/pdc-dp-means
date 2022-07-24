893a8481aec70be3f2915e4f05dee2cdc9af8d34
or dinari <dinari.or@gmail.com>
2022-07-24 21:51:44 +0300

    ddd

--- README.md
+++ README.md
@@ -1,5 +1,53 @@
-# pdc-dp-means: fast and scalable $K$-Means-like clustering when $K$ is unknown
-This is a placeholder for the code (coming soon!) for our UAI 2022 paper, "Revisiting DP-Means: Fast Scalable Algorithms via Parallelism and Delayed Cluster Creation".
-Authors: Or Dinari and Oren Freifeld
+## Revisiting DP-Means: Fast Scalable Algorithms via Parallelism and Delayed Cluster Creation
 
-In short: we provide  *highly scalable and fast* solutions for $K$-Means-like clustering when $K$ is unknown. Our solutions are orders of magnitude faster than the existing approaches (as of June 2022). In terms of performance, our results are usually either better than, or similar to, those of existing approaches. 
+The supplied code has 3 parts -
+
+* The cluster directory, which contains an extension to sklearn with our proposed algorithms, PDC-DP-Means and it's MiniBatch version.
+* the file `date_pdpmeans.py` which contains our implementation of DACE (in three versions, see below) and PDP-Means.
+* Three notebooks which contains the experiment with the other non-parametric methods.
+
+### PDC-DP-Means and MiniBatch PDC-DP-Means
+
+In order to install this, you must clone scikit-learn from: `https://github.com/scikit-learn/scikit-learn.git`.
+
+Navigate the directory `sklearn/cluster`, and replace the files `__init__.py`, `_k_means_lloyd.pyx` and `_kmeans.py` with the respective files under the `cluster` directory.
+Next, you need to install sklearn from source, follow the directions here: https://scikit-learn.org/stable/developers/advanced_installation.html#install-bleeding-edge.
+
+Now, in order to use it, you can simply use `from sklearn.cluster import MiniBatchDPMeans, DPMeans`, the parameters are the same as the `K-Means` counterpart:
+https://scikit-learn.org/stable/modules/generated/sklearn.cluster.KMeans.html
+
+https://scikit-learn.org/stable/modules/generated/sklearn.cluster.MiniBatchKMeans.html
+
+The differences are that instead of number of `n_clusters`, parameter, there is a new parameter `delta` (replacement for lambda, which is reserved in Python),  and in the case of DPMeans, the `algorithm` parameter is removed.
+
+### DACE and PDP-Means
+
+In the file `dace_dpmeans.py` there are 4 relevant algorithms -
+
+`parallel_dp(data,delta,processes,iters)` - PDP-Means , as before, delta replaces lambda, the data is the data, processes is the amount of parallelization, and iters is the maximum iterations (it will stop before if converged).
+
+`DACE(data,delta,num_of_processes)` - The original DACE, as before, delta replaces lambda, the data is the data, num_of_processes is the amount of parallelization.
+
+`DACE_DDP(data,delta,num_of_processes)` - DACE using PDC-DP-Means, but with no inner parallelization.
+
+`DACE_DDP_SPAWN(data,delta,num_of_processes)` - DACE using PDP-DP-Means with inner parallelization, due to different Multi Processing scheme, this might take abit longer to start.
+
+
+Note that in order to run this file some extra dependencies are required, `evaluations.py` file contain several functions, and while some packages required are quite standard - `torchvision,scikit-learn,annoy,pandas,numpy`, it is also required to have a valid `R` enviroment, and the `R` package `maotai` installed, and the python-R interface package `rpy2`.
+
+
+### Experiment notebooks
+We have included the experiments which does not require additional installations apart from the build-from-source scikit-learn, the three attached notebooks are used to recreate the experiments with the other non-parametric methods. Note that the blackbox optimization (while we supplied the code to run it), need to run separately, as it's multiprocess does not play well with Jupyter Notebook. 
+
+
+### Citing this work
+If you use this code for your work, please cite the following:
+
+```
+@inproceedings{dinari2022revisiting,
+  title={Revisiting DP-Means: Fast Scalable Algorithms via Parallelism and Delayed Cluster Creation},
+  author={Dinari, Or and Freifeld, Oren},
+  booktitle={The 38th Conference on Uncertainty in Artificial Intelligence},
+  year={2022}
+}
+```
---
+++ evaluations.py
@@ -0,0 +1,118 @@
+from sklearn.cluster import MiniBatchDPMeans, DPMeans,KMeans, MiniBatchKMeans
+import numpy as np
+from sklearn.datasets import make_blobs
+from sklearn.metrics.pairwise import pairwise_distances_argmin,pairwise_distances
+from sklearn.metrics import normalized_mutual_info_score as nmi
+from sklearn.metrics import silhouette_score as sil
+from sklearn.metrics import adjusted_rand_score as ari
+from sklearn.metrics import pair_confusion_matrix
+from sklearn.model_selection import train_test_split
+from sklearn import metrics
+from matplotlib import pyplot as plt
+import time
+import pandas as pd
+import os
+
+
+NMI = "NMI"
+PURITY = "Purity"
+ARI = "ARI"
+SIL = "Sil"
+FMEASURE = "Fmeasure"
+PRECISION ="precision"
+RECALL = "recall"
+K = "K"
+TIME = "Time"
+
+
+
+def purity_score(y_true, y_pred):
+    contingency_matrix = metrics.cluster.contingency_matrix(y_true, y_pred)
+    return np.sum(np.amax(contingency_matrix, axis=0)) / np.sum(contingency_matrix)
+
+def recall(y_true, y_pred):
+    (tn, fp), (fn, tp) = pair_confusion_matrix(y_true, y_pred)
+    return tp/(tp+fn)
+
+
+def precision(y_true,y_pred):
+    (tn, fp), (fn, tp) = pair_confusion_matrix(y_true, y_pred)
+    return tp/(tp+fp)
+
+def f1(y_true,y_pred):
+    p = precision(y_true,y_pred)
+    r = recall(y_true,y_pred)
+    return (2*p*r)/(p+r)
+
+metrics_dict = {NMI: lambda pred,gt,data:nmi(gt,pred),
+                PURITY : lambda pred,gt,data:purity_score(gt,pred),
+                ARI: lambda pred,gt,data:ari(gt,pred),
+                SIL:lambda pred,gt,data:sil(data,pred),
+                FMEASURE:lambda pred,gt,data:f1(gt,pred),
+                PRECISION:lambda pred,gt,data:precision(gt,pred),
+                RECALL:lambda pred,gt,data:recall(gt,pred),
+                K:lambda pred,gt,data: len(np.unique(pred))
+}
+
+def evaluate_model(model,data,gt,eval_metrics):
+    preds = pairwise_distances_argmin(data, model.cluster_centers_)
+    return {x:metrics_dict[x](preds,gt,data) for x in eval_metrics}
+
+def run_model(model,data,eval_metrics,test_data,test_gt):
+    tic = time.time()
+    m = model.fit(data)
+    toc = time.time()-tic
+    results = evaluate_model(m,test_data,test_gt,eval_metrics)
+    results[TIME] = toc
+    return results
+
+    
+    
+def run_all_models(models_dict,data,test_data,test_gt,count,eval_metrics):
+    results = {}
+    for m,v in models_dict.items():
+        print(f'Running {m}')
+        results[m] = {met:[] for met in (eval_metrics+[TIME])}
+        for i in range(count):
+            print(f'iter {i}/{count}')
+            res = run_model(v(),data,eval_metrics,test_data,test_gt)
+            for k,r in res.items():
+                results[m][k].append(r)
+    mean_stds = {}
+    for m,v in models_dict.items():
+        mean_stds[m] = {met:[] for met in (eval_metrics+[TIME])}
+        for k in mean_stds[m].keys():
+            mean_stds[m][k] = (np.mean(results[m][k]),np.std(results[m][k]))
+    return results,mean_stds
+
+def save_data(data,gt,exp_name):
+    save_path = os.path.join('data',exp_name+'.npy')
+    saved_results = {'data':data,'gt':gt}
+    np.save(save_path,saved_results)
+    
+def load_data(exp_name):
+    save_path =save_path = os.path.join('data',exp_name+'.npy')
+    saved_results = np.load(save_path,allow_pickle=True)
+    # return saved_results
+    return saved_results.item()['data'],saved_results.item()['gt']
+
+def save_results(results,mean_stds,exp_name):
+    save_path = os.path.join('results',exp_name+'.npy')
+    saved_results = {'full':results,'mean_std':mean_stds}
+    np.save(save_path,saved_results)
+    
+def load_results(exp_name):
+    save_path = os.path.join('results',exp_name+'.npy')
+    saved_results = np.load(save_path,allow_pickle=True)
+    # return saved_results
+    return saved_results.item()['full'],saved_results.item()['mean_std']
+
+def train_val_test_split(data,labels,val_frac,test_frac):
+    X_train, X_test, y_train, y_test = train_test_split(data, labels, test_size=test_frac, random_state=1)
+    X_train, X_val, y_train, y_val = train_test_split(X_train, y_train, test_size=val_frac, random_state=1) # 0.25 x 0.8 = 0.2
+    return (X_train,y_train),(X_val,y_val),(X_test,y_test)
+
+
+def calc_objective(model,obj,data,gt):
+    return -metrics_dict[obj](pairwise_distances_argmin(data, model.cluster_centers_),gt,data)
+    
