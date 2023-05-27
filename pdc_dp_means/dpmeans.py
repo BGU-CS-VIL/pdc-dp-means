87669f1b3bfb95bcea12f5a73fdacdb52e4f040c
Or Dinari <dinari.or@gmail.com>
2023-05-27 20:30:30 +0300

    update

--- pdc_dp_means/dpmeans.py
+++ pdc_dp_means/dpmeans.py
@@ -361,15 +361,6 @@ class DPMeans(KMeans):
             best_centers += X_mean
         self.n_clusters = best_centers.shape[0]
         distinct_clusters = len(set(best_labels))
-        if distinct_clusters < self.n_clusters:
-            warnings.warn(
-                "Number of distinct clusters ({}) found smaller than "
-                "n_clusters ({}). Possibly due to duplicate points "
-                "in X.".format(distinct_clusters, self.n_clusters),
-                ConvergenceWarning,
-                stacklevel=2,
-            )
-
         self.cluster_centers_ = best_centers
         self.labels_ = best_labels
         self.inertia_ = best_inertia
@@ -377,130 +368,6 @@ class DPMeans(KMeans):
         # self.iter_time = iter_time
         # self.all_centers = all_centers
         return self
-
-
-def _mini_batch_step(
-    X,
-    x_squared_norms,
-    sample_weight,
-    centers,
-    centers_new,
-    weight_sums,
-    random_state,
-    random_reassign=False,
-    reassignment_ratio=0.01,
-    verbose=False,
-    n_threads=1,
-):
-    """Incremental update of the centers for the Minibatch K-Means algorithm.
-
-    Parameters
-    ----------
-
-    X : {ndarray, sparse matrix} of shape (n_samples, n_features)
-        The original data array. If sparse, must be in CSR format.
-
-    x_squared_norms : ndarray of shape (n_samples,)
-        Squared euclidean norm of each data point.
-
-    sample_weight : ndarray of shape (n_samples,)
-        The weights for each observation in X.
-
-    centers : ndarray of shape (n_clusters, n_features)
-        The cluster centers before the current iteration
-
-    centers_new : ndarray of shape (n_clusters, n_features)
-        The cluster centers after the current iteration. Modified in-place.
-
-    weight_sums : ndarray of shape (n_clusters,)
-        The vector in which we keep track of the numbers of points in a
-        cluster. This array is modified in place.
-
-    random_state : RandomState instance
-        Determines random number generation for low count centers reassignment.
-        See :term:`Glossary <random_state>`.
-
-    random_reassign : boolean, default=False
-        If True, centers with very low counts are randomly reassigned
-        to observations.
-
-    reassignment_ratio : float, default=0.01
-        Control the fraction of the maximum number of counts for a
-        center to be reassigned. A higher value means that low count
-        centers are more likely to be reassigned, which means that the
-        model will take longer to converge, but should converge in a
-        better clustering.
-
-    verbose : bool, default=False
-        Controls the verbosity.
-
-    n_threads : int, default=1
-        The number of OpenMP threads to use for the computation.
-
-    Returns
-    -------
-    inertia : float
-        Sum of squared distances of samples to their closest cluster center.
-        The inertia is computed after finding the labels and before updating
-        the centers.
-    """
-    # Perform label assignment to nearest centers
-    # For better efficiency, it's better to run _mini_batch_step in a
-    # threadpool_limit context than using _labels_inertia_threadpool_limit here
-    labels, inertia = _labels_inertia(
-        X, sample_weight, x_squared_norms, centers, n_threads=n_threads
-    )
-
-    # Update centers according to the labels
-    if sp.issparse(X):
-        _minibatch_update_sparse(
-            X, sample_weight, centers, centers_new, weight_sums, labels, n_threads
-        )
-    else:
-        _minibatch_update_dense(
-            X,
-            sample_weight,
-            centers,
-            centers_new,
-            weight_sums,
-            labels,
-            n_threads,
-        )
-
-    # Reassign clusters that have very low weight
-    if random_reassign and reassignment_ratio > 0:
-        to_reassign = weight_sums < reassignment_ratio * weight_sums.max()
-
-        # pick at most .5 * batch_size samples as new centers
-        if to_reassign.sum() > 0.5 * X.shape[0]:
-            indices_dont_reassign = np.argsort(weight_sums)[int(0.5 * X.shape[0]) :]
-            to_reassign[indices_dont_reassign] = False
-        n_reassigns = to_reassign.sum()
-
-        if n_reassigns:
-            # Pick new clusters amongst observations with uniform probability
-            new_centers = random_state.choice(
-                X.shape[0], replace=False, size=n_reassigns
-            )
-            if verbose:
-                print(f"[MiniBatchKMeans] Reassigning {n_reassigns} cluster centers.")
-
-            if sp.issparse(X):
-                assign_rows_csr(
-                    X,
-                    new_centers.astype(np.intp, copy=False),
-                    np.where(to_reassign)[0].astype(np.intp, copy=False),
-                    centers_new,
-                )
-            else:
-                centers_new[to_reassign] = X[new_centers]
-
-        # reset counts of reassigned centers, but don't reset them too small
-        # to avoid instant reassignment. This is a pretty dirty hack as it
-        # also modifies the learning rates.
-        weight_sums[to_reassign] = np.min(weight_sums[~to_reassign])
-
-    return inertia
 
 
 def _labels_inertia_with_min_sample(
@@ -937,13 +804,6 @@ class MiniBatchDPMeans(KMeans):
             if self._init_size < self.n_clusters:
                 self._init_size = 3 * self.n_clusters
         elif self._init_size < self.n_clusters:
-            warnings.warn(
-                f"init_size={self._init_size} should be larger than "
-                f"n_clusters={self.n_clusters}. Setting it to "
-                "min(3*n_clusters, n_samples)",
-                RuntimeWarning,
-                stacklevel=2,
-            )
             self._init_size = 3 * self.n_clusters
         self._init_size = min(self._init_size, X.shape[0])
 
